f471a30bd28e5e29597bf0293d6d3bdadad7ab3b
Natália Schardosim <nataliaschardosim@MBP-de-Natalia.fritz.box>
2024-02-20 23:06:01 +0100

    algumas mudancas em map is valid, mas precisa de muita correcao

--- map_is_valid.c
+++ map_is_valid.c
@@ -1,13 +1,19 @@
 #include "bsq.h"
 
-void map_is_valid(int fd, int len)
+void map_is_valid(int fd, int len, int height, char **grid)
 {
-    char    *buffer = malloc(len);
+    int i;
+    char    obs;
+    char    empty;
+    char    full;
 
-    while (read(fd, &buffer, 1) == 1 && buffer[len] == '\n')
-	{
-	   
-	}
+    i = 0;
+    while (grid[i][len] == '\n' && len > 3 && height >= 1)
+    {
+        if (check_first_line(grid[0], &obs, &empty, &full))
+    }
+
+    
     else
     {
         write(1, "map error\n", 10);
@@ -16,28 +22,56 @@ void map_is_valid(int fd, int len)
     free(buffer);
 }
 
-void    check_first_line(char *map)
+void    check_first_line(char *line, char *obs, char   *empty, char *full)
 {
-    /*is invalid if a character is missing from the first line, or if two char-
-acters (of empty, full and obstacle) are identical.
+    int i;
+
+    i = 0;
+
+    while (line[i] >= '0' && line[i] >= '9')
+        i++;
+    if (line[i] != line[i + 1] && line[i] != line[i + 2] && line[i + 1] != line[i + 2])
+    {
+        *empty = line[i];
+        *obs = line[i + 1];
+        *full = line[i + 2];
+    }
+    if 
+
+    /*is invalid if a character is missing from the first line
 ◦ The characters can be any printable character, even numbers.*/
 }
 
-void    check_length(char *map)
-{
-    
-//lenghts should be the same
-//strlen until \n
-}
 
-void    check_lines(char *map)
+void    check_char(char *map, char *obs, char   *empty, char *full)
 {
-    // at least one line with 1 box
-    //it needs to end with \n;
-}
+    int i;
 
-void    check_char(char *map)
+    i = 0;
+    j = 0;
+    if (map[i][j] != obs && map[i][j] != empty && map[i][j] != full)
+        return (1); //error
+    else
+        return (0);
+}   
+
+void    map_digits(char *empty, char    *obs, char  *full)
 {
-    //can only be those introduced in the first line.
-}
+    int fd;
+    char    c;
 
+    fd = open("map01.txt", O_RDONLY);
+    if (fd == -1)
+        write(1, "Couldn't open file", 18);
+    while (read(fd, &c, 1) && c != '\n')
+    {
+        *full = c;
+        if (*full)
+        {
+            if (*obs)
+                *empty = *obs;
+            *obs = *full;
+        }
+    }
+    close(fd);
+}
