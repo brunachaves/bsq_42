01c0323a653b48e6f2a4a6df96e7c4cacfd9abb8
Bruna Chaves <bcbs.brunachaves@gmail.com>
2024-02-20 19:38:39 +0100

    create updated, not 100% still

--- create_grid.c
+++ create_grid.c
@@ -1,5 +1,7 @@
 #include "bsq.h"
 
+#define ERROR_GRID "Error: Failed to create the grid.\n"
+
 char    **create_grid(char *buffer)
 {
     int x;
@@ -7,26 +9,31 @@ char    **create_grid(char *buffer)
     int i;
     char    **grid;
 
-     y = buffer[0];
+     y = buffer[0] - 48;
      x = 0;
      while(*buffer != '\n')
+     {
         x++;
+        buffer++;
+     }
+        
     
     grid = malloc(y * sizeof(char *));
     if (grid == NULL) {
-       //ERROR
+        write(1, ERROR_GRID, sizeof(ERROR_GRID) - 1);
         exit(1);
     }
+    i = 0;
     while (i < y)
     {
         grid[i] = malloc(x * sizeof(char));
         if (grid[i] == NULL) {
-            //ERROR
+            write(1, ERROR_GRID, sizeof(ERROR_GRID) - 1);
             exit(1);
         }
         i++;
     }
-    return grid;
+    return (grid);
 }
 
 void    freeGrid(char **grid, int y) {
@@ -41,28 +48,43 @@ void    freeGrid(char **grid, int y) {
     free(grid);
 }
 
-void    populateGrid() 
+void    populateGrid(char *buffer, char **grid) 
 {
-    
-}
+    int i;
+    int j;
 
+    i = 0;
+    buffer += 4;
+    while(*buffer)
+    {
+        j = 0;
+        while(*buffer != '\n')
+        {
+            grid[i][j] = *buffer;
+            j++;
+            buffer++;
+        }
+        i++;
+        buffer++;
+    } 
+}
 
 void    printGrid(char **grid, int x, int y) {
-    for (int i = 0; i < y; i++) {
-        for (int j = 0; j < x; j++) {
-            printf("%c", grid[i][j]);
+    int i;
+    int j;
+    char new_line;
+
+    i = 0;
+    new_line = '\n';
+    while (i < y)
+    {
+        j = 0;
+        while (j < x) 
+        {
+            write(1, grid[i][j], 1);
+            j++;
         }
-        printf("\n");
+        write(1, &new_line, 1);
+        i++;
     }
 }
-
-
-
-
-    /*
-     grid[i][j]: 
-        i - (first item of maps first line -1) [atoi]
-        j - (count until reaches '\n' - 1) [strlen];  
-    */
-
-
